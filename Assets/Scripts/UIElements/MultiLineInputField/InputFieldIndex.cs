5a16bd250e452dbd2c8946d157b094e4430b6d71
mohmehdi <m.m.afkhami@outlook.com>
2020-08-31 22:05:35 +0430

    Multi Item InputField implemented

---
+++ Assets/Scripts/UIElements/MultiLineInputField/InputFieldIndex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldIndex : MonoBehaviour
+{
+    [SerializeField] InputField inputField;
+    [SerializeField] Button removeButton;
+
+    private MultilineInputField parent;
+    public int index=-1;
+    private void Start()
+    {
+        parent = GetComponentInParent<MultilineInputField>();
+
+        inputField.onEndEdit.AddListener(delegate { parent.ChangeInput(inputField); });
+        removeButton.onClick.AddListener(delegate { parent.DeleteItem(index); });
+
+        parent.OnDeleteItem += ReduceIndex;
+    }
+
+    public void ReduceIndex(int i)
+    {
+        if (i<index)
+        {
+            index--;
+        }
+    }
+    private void OnDestroy()
+    {
+        parent.OnDeleteItem -= ReduceIndex;
+    }
+}
---
+++ Assets/Scripts/UIElements/MultiLineInputField/MultilineInputField.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultilineInputField : MonoBehaviour
+{
+    private const int ITEM_HEIGHT = 35;
+
+    [SerializeField] GameObject inputFieldPrefab;
+    [SerializeField] RectTransform addButtonRect;
+    private ScrollRect _scrollView;
+    private List<RectTransform> _itemsTransform;
+    private List<string> _inputs;
+
+    public Action<int> OnDeleteItem;
+
+    private void Start()
+    {
+        _scrollView = GetComponentInChildren<ScrollRect>();
+        _itemsTransform = new List<RectTransform>();
+    }
+
+    public void AddItem()
+    {
+        var ItemRect = Instantiate(inputFieldPrefab,_scrollView.content).GetComponent<RectTransform>();
+        ItemRect.GetComponent<InputFieldIndex>().index = _itemsTransform.Count;
+
+
+        //seting new item position
+        ItemRect.localPosition += Vector3.down * ITEM_HEIGHT * _itemsTransform.Count ;
+        _itemsTransform.Add(ItemRect);
+
+        //set addbutton transform
+        addButtonRect.localPosition += Vector3.down * ITEM_HEIGHT;
+
+        //set Content size
+        _scrollView.content.sizeDelta = new Vector2(0,(_itemsTransform.Count + 1) * ITEM_HEIGHT);
+    }
+
+    public void DeleteItem(int index)
+    {
+        Debug.Log($"Item  {index} Deleted ");
+        Destroy(_itemsTransform[index].gameObject);
+        int i;
+        for (i = index; i < _itemsTransform.Count - 1; i++)
+        {
+            _itemsTransform[i] = _itemsTransform[i + 1];
+            _itemsTransform[i].localPosition += Vector3.up * ITEM_HEIGHT ;
+        }
+        _itemsTransform.RemoveAt(i);
+
+        addButtonRect.localPosition += Vector3.up * ITEM_HEIGHT;
+        OnDeleteItem?.Invoke(index);
+    }
+
+    public void ChangeInput(InputField input)
+    {
+        Debug.Log(input.text);
+    }
+
+}
